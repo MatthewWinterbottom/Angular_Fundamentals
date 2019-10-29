5a4c05bd039f97c046cf819cb51959c8b7b5b2ec
MattWint <mattwint911@gmail.com>
2019-10-29 11:30:56 +0000

    Display product descriptions and add share button

--- angular-fundamentals/src/app/product-list/product-list.component.ts
+++ angular-fundamentals/src/app/product-list/product-list.component.ts
@@ -7,6 +7,8 @@ import { products } from '../products';
   templateUrl: './product-list.component.html',
   styleUrls: ['./product-list.component.css']
 })
+
+
 export class ProductListComponent {
   products = products;
 
@@ -14,10 +16,3 @@ export class ProductListComponent {
     window.alert('The product has been shared!');
   }
 }
-
-
-/*
-Copyright Google LLC. All Rights Reserved.
-Use of this source code is governed by an MIT-style license that
-can be found in the LICENSE file at http://angular.io/license
-*/
