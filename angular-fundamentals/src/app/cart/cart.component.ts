87c4c909aa1a6eaa4659c451003f98317096ace0
MattWint <mattwint911@gmail.com>
2019-10-30 10:20:42 +0000

    Buy products

--- angular-fundamentals/src/app/cart/cart.component.ts
+++ angular-fundamentals/src/app/cart/cart.component.ts
@@ -1,20 +1,31 @@
 import { Component, OnInit } from '@angular/core';
 import { CartService } from '../cart.service';
+import { FormBuilder } from '@angular/forms'
 
 @Component({
     selector: 'app-cart',
     templateUrl: './cart.component.html',
     styleUrls: ['./cart.component.css'],
 })
-export class CartComponent implements OnInit {
+export class CartComponent {
 
     items;
+    checkoutForm;
 
     constructor(
-        private cartService: CartService
-    ) { }
-
-    ngOnInit() {
+        private cartService: CartService,
+        private formBuilder: FormBuilder
+    ) {
         this.items = this.cartService.getItems();
+        this.checkoutForm = this.formBuilder.group({
+            name: '',
+            address: ''
+        });
+    }
+
+    onSubmit(customerData) {
+        window.alert('Congratulation, ' + customerData.name + '. You have made a purchase.')
+        this.items = this.cartService.clearCart();
+        this.checkoutForm.reset();
     }
 }
